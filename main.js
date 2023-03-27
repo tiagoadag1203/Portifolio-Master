11531d89627e76a03c06001c3a41e7323ce11055
Tiago Augusto <tiago.adag@gmail.com>
2023-03-27 10:13:55 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,84 @@
+function sobremim() {
+    var conteudo = document.getElementById("conteudo");
+    var sobremim = document.getElementById("sobremim");
+
+    conteudo.style.display = "none";
+    sobremim.style.display = "block";
+}
+
+
+function skills() {
+    var conteudo = document.getElementById("conteudo");
+    var skills = document.getElementById("skills");
+
+    conteudo.style.display = "none";
+    skills.style.display = "block";
+}
+
+
+function experiencias() {
+    var conteudo = document.getElementById("conteudo");
+    var experiencias = document.getElementById("experiencias");
+
+    conteudo.style.display = "none";
+    experiencias.style.display = "block";
+}
+
+
+function contato() {
+    var conteudo = document.getElementById("conteudo");
+    var contato = document.getElementById("contato");
+
+    conteudo.style.display = "none";
+    contato.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+function voltarsobremim() {
+    var conteudo = document.getElementById("conteudo");
+    var sobremim = document.getElementById("sobremim");
+
+    conteudo.style.display = "block";
+    sobremim.style.display = "none";
+}
+
+function voltarskills() {
+    var conteudo = document.getElementById("conteudo");
+    var skills = document.getElementById("skills");
+
+    conteudo.style.display = "block";
+    skills.style.display = "none";
+}
+
+function voltarexperiencias() {
+    var conteudo = document.getElementById("conteudo");
+    var experiencias = document.getElementById("experiencias");
+
+    conteudo.style.display = "block";
+    experiencias.style.display = "none";
+}
+
+function voltarcontato() {
+    var conteudo = document.getElementById("conteudo");
+    var contato = document.getElementById("contato");
+
+    conteudo.style.display = "block";
+    contato.style.display = "none";
+}
+
+
+
+
+function changeTheme() {
+    var element = document.documentElement;
+    element.classList.toggle("dark");
+}
