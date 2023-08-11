e686ba4107a43b1d9bccac6b05315e85b66b3ac5
Tiago Augusto <tiago.adag@gmail.com>
2023-08-11 14:19:41 -0300

    subindo versao 2.0

--- main.js
+++ main.js
@@ -1,63 +1,41 @@
-function sobremim() {
-    var cont = document.getElementById("cont-sobremim");
-    if (cont.style.display === "flex") {
-        cont.style.display = "none";
-    } else {
-        cont.style.display = "flex";
-    }
-}
-
-function skills() {
-    var cont = document.getElementById("cont-skills");
-    if (cont.style.display === "flex") {
-        cont.style.display = "none";
-    } else {
-        cont.style.display = "flex";
-    }
-}
-
-
-function experiencia() {
-    var cont = document.getElementById("cont-experiencia");
-    if (cont.style.display === "flex") {
-        cont.style.display = "none";
-    } else {
-        cont.style.display = "flex";
-    }
-}
-
-function contato() {
-    var cont = document.getElementById("cont-contato");
-    if (cont.style.display === "flex") {
-        cont.style.display = "none";
-    } else {
-        cont.style.display = "flex";
-    }
-}
-
-function projetos() {
-    var cont = document.getElementById("cont-projetos");
-    if (cont.style.display === "flex") {
-        cont.style.display = "none";
-    } else {
-        cont.style.display = "flex";
-    }
+// script.js
+function openTab(tabId, button) {
+    const tabContents = document.querySelectorAll('.desatived');
+    const tabButtons = document.querySelectorAll('.btn-menu');
+    
+    // Oculta todos os conteúdos das abas
+    tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active');
+    });
+    
+    // Remove a classe .ativo de todos os botões
+    tabButtons.forEach(tabButton => {
+        tabButton.classList.remove('ativo');
+    });
+    
+    // Mostra o conteúdo da aba selecionada
+    const selectedTab = document.getElementById(tabId);
+    selectedTab.classList.add('active');
+    
+    // Encontra o botão correspondente ao ID da aba e adiciona a classe .ativo
+    const activeButton = document.querySelector(`[onclick="openTab('${tabId}')"]`);
+    activeButton.classList.add('ativo');
 }
 
 
 
 function changeTheme() {
     var element = document.documentElement;
-    element.classList.toggle("light");
-    if (element.classList.contains("light")) {
-        localStorage.setItem("theme", "light");
-    } else {
+    element.classList.toggle("dark");
+    if (element.classList.contains("dark")) {
         localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
     }
 }
 
 // Verifica se há um tema armazenado e aplica-o na inicialização
-if (localStorage.getItem("theme") === "light") {
+if (localStorage.getItem("theme") === "dark") {
     var element = document.documentElement;
-    element.classList.add("light");
+    element.classList.add("dark");
 }
